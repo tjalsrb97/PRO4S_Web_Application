e2af94b2815fd8586fa46f3f4114395089cf23cd
KYankee6 <42092864+KYankee6@users.noreply.github.com>
2021-03-30 17:49:56 +0900

    Create my_settings.py

---
+++ mysite/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'webdb',  
+        'USER': 'root',  
+        'PASSWORD': 'password',  
+        'HOST': 'DBIPADDRESS',   
+        'PORT': '3306',  
+    }
+}
