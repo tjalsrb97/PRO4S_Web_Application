284939b2abc65017351783660dd4565e6763f9d2
tjalsrb97 <gnam011@naver.com>
2021-03-30 17:36:49 +0900

    웹 어플리케이션 뼈대 만들기

--- project/urls.py
+++ project/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.introduction, name='introduction'),
+    path('introduction/', views.introduction, name='introduction'),
+    path('research_result/', views.research_result, name='research_result'),
+    path('path_loss_predict/', views.path_loss_predict, name='path_loss_predict'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- project/views.py
+++ project/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
 def introduction(request):
     return render(request, 'project/intro.html', {})
+
+def research_result(request):
+    return render(request, 'project/result.html', {})
+
+def path_loss_predict(request):
+    return render(request, 'project/predict.html', {})
+
+def dashboard(request):
+    return render(request, 'project/board.html', {})
