b45521614a2cfa4d5fd5744e5b2b974ace202110
Kyankee6 <didzl1231@naver.com>
2021-03-30 17:22:38 +0900

    DB settings

---
+++ mysite/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "weDB",
+        "USER": "root",
+        "PASSWORD": "pro4spro4s!!",
+        "HOST": "localhost",
+        "PORT": "3306",
+    }
+}
---
+++ mysite/read.md
@@ -0,0 +1,7 @@
+#양준혁
+
+#파이썬에서는 `mysqlclient`라는 라이브러리를 통해 MySQL과 DB를 연동할 수 있다.
+
+```
+$ pip install mysqlclient
+```
