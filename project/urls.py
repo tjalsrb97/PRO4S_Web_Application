883457a527955ce671dc4c9d3db6f07720152975
Kyankee6 <didzl1231@naver.com>
2021-03-31 18:36:37 +0900

    load models

--- project/urls.py
+++ project/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('introduction/', views.introduction, name='introduction'),
-    path('research_result/', views.research_result, name='research_result'),
-    path('path_loss_predict/', views.path_loss_predict, name='path_loss_predict'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path("introduction/", views.introduction, name="introduction"),
+    path("research_result/", views.research_result, name="research_result"),
+    path("path_loss_predict/", views.path_loss_predict, name="path_loss_predict"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("", views.introduction, name="introduction"),
 ]
