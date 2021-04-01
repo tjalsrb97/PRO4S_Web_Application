d281acb57cc7ce06b3033777cda276fa93f0b616
tjalsrb97 <gnam011@naver.com>
2021-04-01 18:17:24 +0900

    진짜 될거야

--- project/views.py
+++ project/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from .models import User
 
 def introduction(request):
     return render(request, 'project/intro.html', {})
